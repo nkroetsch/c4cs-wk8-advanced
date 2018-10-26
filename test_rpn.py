df96043b277811bed081b3b0e6457debcca8ec46
Nicholas <kroetsch@umich.edu>
2018-10-26 11:27:35 -0400

    implement subtract

--- test_rpn.py
+++ test_rpn.py
@@ -7,5 +7,5 @@ class TestBasics(unittest.TestCase):
          self.assertEqual(2, result)
 
     def test_sub(self):
-         result = rpn.calculate('1 1 -')
-         self.assertEqual(0, result)
+         result = rpn.calculate('4 3 -')
+         self.assertEqual(1, result)
